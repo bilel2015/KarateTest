c3760d5fc6a1ddcd95cbc7b8b1a6906362848e57
bilel2015 <bilelelaifi@gmail.com>
2018-07-05 16:36:56 +0200

    project init

---
+++ src/main/java/org/sid/KarateTestApplication.java
@@ -0,0 +1,12 @@
+package org.sid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KarateTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KarateTestApplication.class, args);
+	}
+}
